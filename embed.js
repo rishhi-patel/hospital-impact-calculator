39ea71b02b7ae151c77436228a9cfaa907fa0053
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2025-03-12 15:03:08 -0400

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  console.log("Embed.js executed!")
   await initCalculator()
 })
 
 async function initCalculator() {
-  console.log("Initializing calculator...")
-
   // Fetch and inject HTML structure from index.html
   const response = await fetch(
     "https://rishhi-patel.github.io/hospital-impact-calculator/index.html"
@@ -17,7 +14,6 @@ async function initCalculator() {
   container.id = "hospital-impact-calculator"
   container.innerHTML = html
   document.body.appendChild(container)
-  console.log("Calculator HTML injected")
 
   // Load Tailwind CSS using a proper <link> tag
   const tailwindLink = document.createElement("link")
@@ -25,7 +21,6 @@ async function initCalculator() {
   tailwindLink.href =
     "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" // Using CDN for Tailwind CSS
   document.head.appendChild(tailwindLink)
-  console.log("Tailwind CSS loaded")
 
   // Load main.js
   const script = document.createElement("script")
@@ -33,7 +28,6 @@ async function initCalculator() {
     "https://rishhi-patel.github.io/hospital-impact-calculator/js/main.js"
   script.defer = true
   script.onload = () => {
-    console.log("Main.js injected!")
     // Ensure Tailwind applies styles properly after script loads
     setTimeout(() => {
       tailwind.config = {
